98198552ee038384bbfd197835a94956f28f4159
Annika Chauhan <annikachauhan@Annikas-MacBook-Pro.local>
2020-03-07 23:40:51 +0000

    other slight cosmetic changes to cards and header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.scss";
 export default class Header extends Component {
     render(){
         return(
-            <h1 className={styles.header}>Pokedex</h1>
+        <h1 className={styles.header}>Pokédex</h1>
         );
     }
 }
